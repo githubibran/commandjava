ab4be1531627d9c11c4cded8b12238f876ec6894
githubibran <ibran@retigence.com>
2012-09-17 15:36:23 +0530

    deleted one row and added on row

--- Commandpromt.java
+++ Commandpromt.java
@@ -7,7 +7,7 @@ public class Commandpromt {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ibran");
+		System.out.println("retigence");
 		System.out.println("command");
 	}
 
